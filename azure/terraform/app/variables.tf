350a568a31f0ba9ffbbfffdd5d062501cdd32bef
Manuel Gil <manuelgilsitio@gmail.com>
2025-12-27 11:51:01 -0500

    update

--- azure/terraform/app/variables.tf
+++ azure/terraform/app/variables.tf
@@ -2,3 +2,8 @@ variable "image_name" {
   description = "Name of the docker image"
   type        = string
 }
+
+variable "azure_container_app_name" {
+  description = "Name of the azure container app"
+  type        = string
+}
