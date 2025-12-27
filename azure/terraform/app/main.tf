cea3c9b75923458dfec03d6ce42291d4da3ad55b
Manuel Gil <manuelgilsitio@gmail.com>
2025-12-26 23:41:55 -0500

    update

--- azure/terraform/app/main.tf
+++ azure/terraform/app/main.tf
@@ -1,4 +1,14 @@
 
+data "terraform_remote_state" "core" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "gilsamasstudyapitfstate"
+    storage_account_name = "gilsamastfstatestg"
+    container_name       = "mlflowcloudtfstate"
+    key                  = "terraform.core.tfstate"
+  }
+}
+
 resource "azurerm_container_app" "res-3" {
   container_app_environment_id = data.terraform_remote_state.core.outputs.container_app_env_id
   name                         = var.azure_container_app_name
