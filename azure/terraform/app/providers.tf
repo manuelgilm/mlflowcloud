7a493563b63aac32f6ee2a1f40285d351be0e62f
Manuel Gil <manuelgilsitio@gmail.com>
2025-12-26 20:56:36 -0500

    Udpating app module

---
+++ azure/terraform/app/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.0.0"
+  backend "azurerm" {
+    resource_group_name  = "gilsamasstudyapitfstate"
+    storage_account_name = "gilsamastfstatestg"
+    container_name       = "mlflowcloudtfstate"
+    key                  = "terraform.app.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.47.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ azure/terraform/app/variables.tf
@@ -0,0 +1,4 @@
+variable "image_name" {
+  description = "Name of the docker image"
+  value       = string
+}
