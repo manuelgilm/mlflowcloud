8a90f4d7e1635da04c412b00138074d158a18e74
Manuel Gil <manuelgilsitio@gmail.com>
2026-01-01 16:31:35 -0500

    enabling publisc network

--- azure/terraform/core/main.tf
+++ azure/terraform/core/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_container_registry" "res-1" {
   name                          = var.acr_name
   network_rule_bypass_option    = "AzureServices"
   network_rule_set              = []
-  public_network_access_enabled = false
+  public_network_access_enabled = true
   quarantine_policy_enabled     = false
   resource_group_name           = azurerm_resource_group.res-0.name
   retention_policy_in_days      = 0
