577ece21c1c4cb7cabbf7215b499cfe58fd99350
Manuel Gil <manuelgilsitio@gmail.com>
2026-01-01 17:01:03 -0500

    importin app state

--- azure/terraform/app/main.tf
+++ azure/terraform/app/main.tf
@@ -9,6 +9,16 @@ data "terraform_remote_state" "core" {
   }
 }
 
+data "terraform_remote_state" "app" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "gilsamasstudyapitfstate"
+    storage_account_name = "gilsamastfstatestg"
+    container_name       = "mlflowcloudtfstate"
+    key                  = "terraform.app.tfstate"
+  }
+}
+
 resource "azurerm_container_app" "res-3" {
   container_app_environment_id = data.terraform_remote_state.core.outputs.container_app_env_id
   name                         = var.azure_container_app_name
