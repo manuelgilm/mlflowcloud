f47fb43f933ef812d7a38eb2a7e86d86bf325c5f
Manuel Gil <manuelgilsitio@gmail.com>
2026-01-01 16:51:56 -0500

    change to username and password

--- azure/terraform/app/main.tf
+++ azure/terraform/app/main.tf
@@ -17,8 +17,9 @@ resource "azurerm_container_app" "res-3" {
   tags                         = {}
   workload_profile_name        = "Consumption"
   registry {
-    identity = "system-environment"
-    server   = data.terraform_remote_state.core.outputs.acr_login_server
+    password_secret_name = data.terraform_remote_state.core.outputs.acr_login_password
+    server               = data.terraform_remote_state.core.outputs.acr_login_server
+    username             = data.terraform_remote_state.core.outputs.acr_login_username
   }
   ingress {
     allow_insecure_connections = false
