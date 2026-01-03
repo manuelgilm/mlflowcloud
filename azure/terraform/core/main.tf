f8b4c97d0c6d8513c2ed4ea55213b43769483575
Manuel Gil <manuelgilsitio@gmail.com>
2026-01-03 13:10:39 -0500

    adding networking

--- azure/terraform/core/main.tf
+++ azure/terraform/core/main.tf
@@ -5,6 +5,42 @@ resource "azurerm_resource_group" "res-0" {
     "environment" = "dev"
   }
 }
+
+# Virtual Network for secure internal communication
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.resource_group_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.res-0.name
+  tags = {
+    "environment" = "dev"
+  }
+}
+
+# Subnet for Container Apps Environment
+resource "azurerm_subnet" "container_apps_subnet" {
+  name                 = "container-apps-subnet"
+  resource_group_name  = azurerm_resource_group.res-0.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/23"]
+}
+
+# Subnet for PostgreSQL private endpoint
+resource "azurerm_subnet" "postgresql_subnet" {
+  name                 = "postgresql-subnet"
+  resource_group_name  = azurerm_resource_group.res-0.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Subnet for Storage Account private endpoint
+resource "azurerm_subnet" "storage_subnet" {
+  name                 = "storage-subnet"
+  resource_group_name  = azurerm_resource_group.res-0.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
 resource "azurerm_container_registry" "res-1" {
   admin_enabled                 = true
   anonymous_pull_enabled        = false
@@ -26,11 +62,13 @@ resource "azurerm_container_registry" "res-1" {
 }
 
 resource "azurerm_container_app_environment" "res-2" {
-  location                   = var.location
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.res-4.id
-  name                       = var.azure_container_app_env_name
-  resource_group_name        = azurerm_resource_group.res-0.name
-  tags                       = {}
+  location                       = var.location
+  log_analytics_workspace_id     = azurerm_log_analytics_workspace.res-4.id
+  name                           = var.azure_container_app_env_name
+  resource_group_name            = azurerm_resource_group.res-0.name
+  infrastructure_subnet_id       = azurerm_subnet.container_apps_subnet.id
+  internal_load_balancer_enabled = false  # Set to false to allow public ingress for MLflow
+  tags                           = {}
   workload_profile {
     maximum_count         = 0
     minimum_count         = 0
@@ -72,7 +110,7 @@ resource "azurerm_storage_account" "res-5" {
   min_tls_version                   = "TLS1_2"
   name                              = var.azure_storage_account_name
   nfsv3_enabled                     = false
-  public_network_access_enabled     = true
+  public_network_access_enabled     = false
   queue_encryption_key_type         = "Service"
   resource_group_name               = azurerm_resource_group.res-0.name
   sftp_enabled                      = false
@@ -113,7 +151,7 @@ resource "azurerm_postgresql_flexible_server" "res-7" {
   geo_redundant_backup_enabled  = false
   location                      = var.location
   name                          = var.postgresql_flexible_server_name
-  public_network_access_enabled = true
+  public_network_access_enabled = false
   resource_group_name           = azurerm_resource_group.res-0.name
   sku_name                      = "B_Standard_B2s"
   storage_mb                    = 32768
@@ -125,5 +163,91 @@ resource "azurerm_postgresql_flexible_server" "res-7" {
     active_directory_auth_enabled = true
     password_auth_enabled         = true
     tenant_id                     = var.tenant_id
+  }
+}
+
+# Private DNS Zone for PostgreSQL
+resource "azurerm_private_dns_zone" "postgresql_dns" {
+  name                = "privatelink.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.res-0.name
+  tags = {
+    "environment" = "dev"
+  }
+}
+
+# Link Private DNS Zone to VNet
+resource "azurerm_private_dns_zone_virtual_network_link" "postgresql_dns_link" {
+  name                  = "postgresql-dns-link"
+  resource_group_name   = azurerm_resource_group.res-0.name
+  private_dns_zone_name = azurerm_private_dns_zone.postgresql_dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  tags = {
+    "environment" = "dev"
+  }
+}
+
+# Private Endpoint for PostgreSQL
+resource "azurerm_private_endpoint" "postgresql_endpoint" {
+  name                = "${var.postgresql_flexible_server_name}-endpoint"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.res-0.name
+  subnet_id           = azurerm_subnet.postgresql_subnet.id
+  tags = {
+    "environment" = "dev"
+  }
+
+  private_service_connection {
+    name                           = "${var.postgresql_flexible_server_name}-connection"
+    private_connection_resource_id = azurerm_postgresql_flexible_server.res-7.id
+    is_manual_connection           = false
+    subresource_names              = ["postgresqlServer"]
+  }
+
+  private_dns_zone_group {
+    name                 = "postgresql-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.postgresql_dns.id]
+  }
+}
+
+# Private DNS Zone for Storage Account (Blob)
+resource "azurerm_private_dns_zone" "storage_blob_dns" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.res-0.name
+  tags = {
+    "environment" = "dev"
+  }
+}
+
+# Link Storage Blob DNS Zone to VNet
+resource "azurerm_private_dns_zone_virtual_network_link" "storage_blob_dns_link" {
+  name                  = "storage-blob-dns-link"
+  resource_group_name   = azurerm_resource_group.res-0.name
+  private_dns_zone_name = azurerm_private_dns_zone.storage_blob_dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  tags = {
+    "environment" = "dev"
+  }
+}
+
+# Private Endpoint for Storage Account (Blob)
+resource "azurerm_private_endpoint" "storage_blob_endpoint" {
+  name                = "${var.azure_storage_account_name}-blob-endpoint"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.res-0.name
+  subnet_id           = azurerm_subnet.storage_subnet.id
+  tags = {
+    "environment" = "dev"
+  }
+
+  private_service_connection {
+    name                           = "${var.azure_storage_account_name}-blob-connection"
+    private_connection_resource_id = azurerm_storage_account.res-5.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+
+  private_dns_zone_group {
+    name                 = "storage-blob-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.storage_blob_dns.id]
   }
 }
